0b36cee1b58e817f95188e1435a752bd36c41c64
hugordm <hugomelo742@gmail.com>
2025-05-27 15:17:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("todo-form");
+
+const input = document.getElementById("todo-input");
+
+const list = document.getElementById("todo-list");
+
+form.addEventListener("submit", function(evento){
+    evento.preventDefault();
+
+    const task = input.value.trim();
+    if (task !==""){
+        const li = document.createElement("li");
+        li.textContent = task;
+
+        const deletBtn = document.createElement("button");
+        deletBtn.textContent = "excluir";
+
+        deletBtn.addEventListener("click", function(){
+            list.removeChild(li);
+        });
+        li.appendChild(deletBtn);
+        list.appendChild(li);
+        input.value = "";
+    }
+});
